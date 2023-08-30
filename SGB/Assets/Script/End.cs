46068abd80d562d50d38f0767c133063cceb3491
sou-2626 <135081708+sou-2626@users.noreply.github.com>
2023-08-30 10:36:07 +0900

    tui

---
+++ SGB/Assets/Script/End.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    public void SwitchScene()
+    {
+        SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
